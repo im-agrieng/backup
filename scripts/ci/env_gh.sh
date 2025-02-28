f9c02bf7638d11b9d9d3147cb251f4b2993567d2
im-agrieng <im.agri.tools@gmail.com>
2025-02-28 23:46:59 +0100

    0

--- scripts/ci/env_gh.sh
+++ scripts/ci/env_gh.sh
@@ -73,8 +73,8 @@ if [[ ${ALL_FILES_ACCESS} == "ON" ]]; then
 	export APP_PACKAGE_NAME="qfield_all_access${APP_PACKAGE_NAME_SUFFIX}"
 	export CI_PACKAGE_NAME="qfield_all_access"
 else
-	export APP_PACKAGE_NAME="qfield${APP_PACKAGE_NAME_SUFFIX}"
-	export CI_PACKAGE_NAME="qfield"
+	export APP_PACKAGE_NAME="sigpac-god${APP_PACKAGE_NAME_SUFFIX}"
+	export CI_PACKAGE_NAME="sigpac"
 fi
 
 {
